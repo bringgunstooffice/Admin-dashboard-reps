ba97cee37a79881ec68883aee70c9d1029481fa5
Farid Mufti <37999465+bringgunstooffice@users.noreply.github.com>
2018-07-26 11:39:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>SP Syarif Hidayatullah</title>
+
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="dist/css/admin.css">
+</head>
+<body>
+	<nav class="navbar navbar-custom navbar-static-top">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
+				<!--Konten Brand-->
+				<a class="navbar-brand" href="#"><img src="dist/sp-sarip.png" alt="SP Syarif">
+        </a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+
+
+				</ul>
+
+				<ul class="nav navbar-nav navbar-right">
+
+					<!--Dropdown akun di navbar-->
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle"></i> Akun <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#"><i class="fa fa-user"></i> Profile</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#"><i class="fa fa-sign-out"></i> Logout</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+
+ 
+
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+
+
+				</ul>
+
+
+
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+
+
+
+</div>		
+<div class="wrapper">
+</div>
+
+
+<aside class="sidebar">
+	<div class="menu">
+		<ul class="menu-content">
+			<ul>
+				<li>
+					<a href="index.php"> <i class="fa fa-home"></i> <span> Home</span></a>
+				</li>
+						<!--<li>
+							<a href="#"><i class="fa fa-database"></i> <span> Data Master</span><i class="fa fa-angle-down pull-right"></i></a>
+						</li>--> 
+						<li>
+							<a href="master_buku.php"><i class="fa fa-book"></i> <span> Data Master Buku</span></a>
+						</li>
+						<li>
+							<a href="master_anggota.php"><i class="fa fa-users"></i> <span> Data Master Anggota</span></a>
+						</li>					
+						<li>
+							<a href="transaksi.php"><i class="fa fa-shopping-cart"></i> <span> Transaksi</span></a>
+						</li>
+						
+					</ul>
+				</div>
+			</aside>
+			<section class="content">
+				<div class="inner"></div>
+				<p>
+					<h2><strong>Selamat Datang di Aplikasi Sistem Perpustakaan Syarif Hidayatullah !</h2></strong>
+					Sistem ini dibuat guna untuk memenuhi tugas akhir, dan semoga nantinya sistem ini dapat mempermudah pihak sekolah SMA Syarif Hidayatullah untuk melakukan pencatatan atau <em>monitoring</em> semua buku dan anggotanya di perpustakaan SMA Syarif Hidayatullah.
+					<br>
+					<br>
+					<strong>Under License</strong><br>
+					Wahyu TJ / Stak Studio
+					<br>
+					Beta Version 0.1
+					<br>
+					<br>
+					<strong>E-Mail</strong>
+					<br>
+					<a href="#">v3-pinki@gmail.com</a>
+
+
+
+
+				</p>
+			</section>
+		</div>
+
+		<script src="dist/js/jquery.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+		<script src="dist/js/admin.js"></script>
+	</body>
+	</html>
